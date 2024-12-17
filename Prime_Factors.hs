dfc921ebdce03bc095beee8113879506682c0601
SamE-345 <sameveritt66@gmail.com>
2024-12-17 16:31:00 +0000

    Finished Program

---
+++ Prime_Factors.hs
@@ -0,0 +1,17 @@
+isFactor :: Int -> Int -> Bool
+isFactor x y = x `mod` y == 0
+
+isPrime ::  Int -> Bool
+isPrime x = (power 2 (x-1)) `mod` x == 1
+
+power :: Int -> Int -> Int
+power x y 
+    | y == 0 = 1
+    | otherwise = x* (power x (y-1))
+
+primeFactors :: Int -> [Int]
+primeFactors x = filter (isFactor x) (filter(isPrime) [2..x])
+
+main :: IO()
+main = do
+    print $ primeFactors 15
